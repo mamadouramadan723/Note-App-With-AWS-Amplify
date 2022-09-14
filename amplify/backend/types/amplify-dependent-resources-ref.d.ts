4c2f3574c2c8fa3ccf97f7d357ab82e26cda3386
RMD <72153331+mamadouramadan723@users.noreply.github.com>
2022-09-14 22:12:35 +0100

    3 - Amplify Backend Auth Config

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "noteappawsamplifyce9d80d0": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- app/src/main/java/com/rmd/awsamplify/noteapp/MainActivity.kt
+++ app/src/main/java/com/rmd/awsamplify/noteapp/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.rmd.awsamplify.noteapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.rmd.awsamplify.noteapp.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +24,19 @@ class MainActivity : AppCompatActivity() {
 
         // prepare our List view and RecyclerView (cells)
         setupRecyclerView(item_list)
+        //
+        setupAuthButton(UserData)
+
+        UserData.isSignedIn.observe(this) { isSignedUp ->
+            // update UI
+            Log.i(TAG, "isSignedIn changed : $isSignedUp")
+
+            if (isSignedUp) {
+                fabAuth.setImageResource(R.drawable.ic_baseline_lock_open)
+            } else {
+                fabAuth.setImageResource(R.drawable.ic_baseline_lock)
+            }
+        }
     }
     // recycler view is the list of cells
     private fun setupRecyclerView(recyclerView: RecyclerView) {
@@ -34,8 +50,30 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun setupAuthButton(userData: UserData) {
+
+        // register a click listener
+        fabAuth.setOnClickListener { view ->
+
+            val authButton = view as FloatingActionButton
+
+            if (userData.isSignedIn.value!!) {
+                authButton.setImageResource(R.drawable.ic_baseline_lock_open)
+                Backend.signOut()
+            } else {
+                authButton.setImageResource(R.drawable.ic_baseline_lock_open)
+                Backend.signIn(this)
+            }
+        }
+    }
+
+    // receive the web redirect after authentication
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Backend.handleWebUISignInResponse(requestCode, resultCode, data)
+    }
+
     companion object {
         private const val TAG = "MainActivity"
     }
-
 }
